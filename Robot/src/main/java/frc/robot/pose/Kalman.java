f8b0099c34a8325f8ef686ff4d05ba3dedf45e17
AgilanGu <aguquantum@gmail.com>
2020-07-06 14:46:39 -0400

    added state2d

--- Robot/src/main/java/frc/robot/pose/Kalman.java
+++ Robot/src/main/java/frc/robot/pose/Kalman.java
@@ -1,15 +1,21 @@
 package frc.robot.pose;
 
-import org.apache.commons.math3.*;
+import org.apache.commons.math3.filter.DefaultMeasurementModel;
+import org.apache.commons.math3.filter.DefaultProcessModel;
+import org.apache.commons.math3.filter.KalmanFilter;
+import org.apache.commons.math3.filter.MeasurementModel;
+import org.apache.commons.math3.filter.ProcessModel;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
-
-import edu.wpi.first.wpilibj.geometry.Transform2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.util.MathArrays.Position;
+import org.apache.commons.math3.geometry.euclidean.twod.*;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Twist2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import java.math.*;
+import edu.wpi.first.wpilibj.trajectory.Trajectory.State;
+import frc.robot.pose.State2d;
+
+import frc.robot.Constants;
 
 
 public class Kalman {
@@ -17,10 +23,29 @@ public class Kalman {
     //Timescale for integration
     double dt = .1;
     //Acceleration equation
-    double accel = .5 * Math.pow(dt, 2);
+    double accel = (1/2) * Math.pow(dt, 2);
     //Jerk equation
     double jerk = (1/3) * Math.pow(dt, 3);
  
+    //A - state transition matrix
+    private RealMatrix A;
+    //B - control input matrix
+    private RealMatrix B;
+    //H - measurement matrix
+    private RealMatrix H;
+    //Q - process noise covariance matrix (error in the process)
+    private RealMatrix Q;
+    //R - measurement noise covariance matrix (error in the measurement)
+    private RealMatrix R;
+    //PO - error covariance matrix
+    private RealMatrix PO;
+    //x state
+    private RealVector x;
+    //Filter
+    private KalmanFilter filter;
+
+
+    public Kalman(){
     //A matrix, state transition matrix:
     //
     //[ 1, 0, 0, 0, 1/2(dt)^2, 0 ]
@@ -31,7 +56,7 @@ public class Kalman {
     //[ 0, 0, 0, 0, 0, 1]
 
     RealMatrix A = new Array2DRowRealMatrix(new double[][] { { 1, 0, 0, 0, accel, 0 },
-                                                             { 0, 1, dt, 0, 0, 0, accel}, 
+                                                             { 0, 1, dt, 0, 0, accel}, 
                                                              { 0, 0, 1, dt, 0, 0}, 
                                                              { 0, 0, 0, 1, dt, 0},
                                                              { 0, 0, 0, 0, 1, dt},
@@ -54,14 +79,90 @@ public class Kalman {
                                                              { dt},
                                                              { dt}});
 
-    //H matrix, measurement matrix
-    
+    //H matrix, measurement matrix:
+    //
+    //[ 1, 0, 0, 0, 0, 0]
+    //[ 0, 1, 0, 0, 0, 0]
+    //[ 0, 0, 1, 0, 0, 0]
+    //[ 0, 0, 0, 1, 0, 0]
+    //[ 0, 0, 0, 0, 1, 0]
+    //[ 0, 0, 0, 0, 0, 1]
+
+    RealMatrix H = new Array2DRowRealMatrix(new double[][] { { 1, 0, 0, 0, 0, 0},
+                                                             { 0, 1, 0, 0, 0, 0}, 
+                                                             { 0, 0, 1, 0, 0, 0}, 
+                                                             { 0, 0, 0, 1, 0, 0},
+                                                             { 0, 0, 0, 0, 1, 0},
+                                                             { 0, 0, 0, 0, 0, 1}});
+
+    // X vector, initial state vector:
+    //
+    //[ 0, 0, 0, 0, 0, 0]
+
+    RealVector x = new ArrayRealVector(new double[] { 0, 0, 0, 0, 0, 0 });
+
+    //Q matrix, process noise covariance matrix
+    //
+    //[ 1, 0, 0, 0, 0, 0]
+    //[ 0, 1, 0, 0, 0, 0]
+    //[ 0, 0, 1, 0, 0, 0]
+    //[ 0, 0, 0, 1, 0, 0]
+    //[ 0, 0, 0, 0, 1, 0]
+    //[ 0, 0, 0, 0, 0, 1]
+
+    RealMatrix Q = new Array2DRowRealMatrix(new double[][] { { 1, 0, 0, 0, 0, 0},
+                                                             { 0, 1, 0, 0, 0, 0}, 
+                                                             { 0, 0, 1, 0, 0, 0}, 
+                                                             { 0, 0, 0, 1, 0, 0},
+                                                             { 0, 0, 0, 0, 1, 0},
+                                                             { 0, 0, 0, 0, 0, 1}});
+
+    //R matrix, measurement noise covariance matrix
+    //
+    //[ 1, 0, 0, 0, 0, 0]
+    //[ 0, 1, 0, 0, 0, 0]
+    //[ 0, 0, 1, 0, 0, 0]
+    //[ 0, 0, 0, 1, 0, 0]
+    //[ 0, 0, 0, 0, 1, 0]
+    //[ 0, 0, 0, 0, 0, 1]
+
+    RealMatrix R = new Array2DRowRealMatrix(new double[][] { { 1, 0, 0, 0, 0, 0},
+                                                             { 0, 1, 0, 0, 0, 0}, 
+                                                             { 0, 0, 1, 0, 0, 0}, 
+                                                             { 0, 0, 0, 1, 0, 0},
+                                                             { 0, 0, 0, 0, 1, 0},
+                                                             { 0, 0, 0, 0, 0, 1}});
+
+    //P matrix, error covariance matrix
+    //
+    //[ 1, 1, 1, 1, 1, 1]
+    //[ 1, 1, 1, 1, 1, 1]
+    //[ 1, 1, 1, 1, 1, 1]
+    //[ 1, 1, 1, 1, 1, 1]
+    //[ 1, 1, 1, 1, 1, 1]
+    //[ 1, 1, 1, 1, 1, 1]
+
+    RealMatrix PO = new Array2DRowRealMatrix(new double[][] { { 1, 1, 1, 1, 1, 1},
+                                                              { 1, 1, 1, 1, 1, 1}, 
+                                                              { 1, 1, 1, 1, 1, 1}, 
+                                                              { 1, 1, 1, 1, 1, 1},
+                                                              { 1, 1, 1, 1, 1, 1},
+                                                              { 1, 1, 1, 1, 1, 1}});
+
+    ProcessModel pm = new DefaultProcessModel(A, B, Q, x, PO);
+    MeasurementModel mm = new DefaultMeasurementModel(H, R);
+    filter = new KalmanFilter(pm, mm);
+    }
+
+    public State2d estimateState(State2d vision2d, State2d vision3d, State2d IMU, State2d encoderOdometry) {
+    State2d estimation = new State2d();
 
 
 
+    return estimation;
 
-
-
+    }
 
 
 }
+
---
+++ Robot/src/main/java/frc/robot/pose/State2d.java
@@ -0,0 +1,74 @@
+package frc.robot.pose;
+
+import edu.wpi.first.wpilibj.trajectory.Trajectory.State;
+
+public class State2d{
+
+private final double xPos;
+private final double yPos;
+private final double xVel;
+private final double yVel;
+private final double xAccel;
+private final double yAccel;
+
+public State2d(){
+xPos = 0;
+yPos = 0;
+xVel = 0;
+yVel = 0;
+xAccel = 0;
+yAccel = 0;
+
+}
+
+public State2d(double xpos, double ypos, double xvel, double yvel, double xaccel, double yaccel) {
+xPos = xpos;
+yPos = ypos;
+xVel = xvel;
+yVel = yvel;
+xAccel = xaccel;
+yAccel = yaccel;
+}
+
+public State2d(double xpos, double ypos, double xvel, double yvel) {
+    xPos = xpos;
+    yPos = ypos;
+    xVel = xvel;
+    yVel = yvel;
+    xAccel = 0;
+    yAccel = 0;
+}
+
+public State2d(double xpos, double ypos) {
+    xPos = xpos;
+    yPos = ypos;
+    xVel = 0;
+    yVel = 0;
+    xAccel = 0;
+    yAccel = 0;
+}
+
+public double getXPos(){
+    return xPos;
+}
+public double getYPos(){
+    return yPos;
+}
+public double getXVel(){
+    return xVel;
+}
+public double getYVel(){
+    return yVel;
+}
+public double getXAccel(){
+    return xAccel;
+}
+public double getYAccel(){
+    return yAccel;
+}
+
+
+
+
+
+}
